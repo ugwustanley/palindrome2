e9b5719a62c29935059793ca2a73ec96695e2d09
ugwustanley <ugwuchiagoziestanley@gmail.com>
2021-05-26 20:14:36 +0100

    add app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+const palindrome = (string) =>{
+    //create regex for any pattern other than a string or letter  
+    const regex = /\W|_/g
+    //implement your regex removing all none strings and characters
+    const newString = string.replace(regex , "").toLowerCase()
+    console.log(newString)
+    //reverse your new string 
+    const reversedString = newString.split('').reverse().join('')
+    //check if reversed string is same as new string
+    if(newString === reversedString){
+        return true
+    }
+    return false
+    
+}
+
+console.log(palindrome('__%$#@bAggab__'))
+console.log(palindrome('eye'))
